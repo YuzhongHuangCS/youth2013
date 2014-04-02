8ce4246baf0ab82863fd2a7b3be0e003c08c9420
PillowSky <ihyzi@163.com>
2014-04-03 02:56:05 +0800

    nightly version

---
+++ detail.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>2013年度浙江大学“青年岗位能手”事迹展</title>
+	<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+	<div id="nav">
+		<a href="http://www.youth.zju.edu.cn/" id="link">
+			<span>求是青年 | 共青团浙江大学委员会</span>
+		</a>
+	</div>
+	<img src="img/banner.jpg" id="banner">
+
+	<div id="detail">
+		<div id="detailList">
+			<p>2013年度青年岗位能手</p>
+			<hr />
+			<ul>
+				<li>吕超峰</li>
+				<li>吕超峰</li>
+				<li>吕超峰</li>
+				<li>吕超峰</li>
+				<li>吕超峰</li>
+				<li>吕超峰</li>
+			</ul>
+		</div>
+		<div id="detailBody">
+		</div>
+	</div>
+	<script src="js/jquery-2.0.3.min.js"></script>
+</body>
+</html>
